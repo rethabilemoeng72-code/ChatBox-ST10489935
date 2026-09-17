06935fb4428d31e14666d55ea60ada7af8e95599
rethabilemoeng-72code <rethabilemoeng72@gmail.com>
2026-09-17 15:53:58 +0200

    I am finished with Login.java file

--- src/main/java/com/mycompany/chatbox/st10489935/Login.java
+++ src/main/java/com/mycompany/chatbox/st10489935/Login.java
@@ -9,5 +9,52 @@ package com.mycompany.chatbox.st10489935;
  * @author Student
  */
 public class Login {
+    private String storedUsername;
+    private String storedPassword;
+    private String storedFirstName;
+    private String storedLastName;
     
+    public boolean  checkUserName (String username) {
+        return username.contains("_") && username.length() <=5;
+    }
+    
+    public boolean checkPasswordComplexity (String password) {
+        return password.length() >= 8 &&
+               password.matches(".*[A-Z].*") &&
+               password.matches(".*[0-9].*") &&
+               password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};'\"\\\\|,.<>\\/?].*");
+    }
+    
+    public boolean checkCellPhoneNumber (String cellNumber) {
+        return cellNumber.matches("^\\+\\d{1,3}\\d{1,10}$");
+    }
+    
+    public String registerUser(String username, String password, String firstName, String lastName) {
+        if (!checkUserName (username)) {
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is not more than five characters in length.";
+        }
+        if (!checkPasswordComplexity (password)) {
+            return "Password is not correctly formatted; please ensure it contains atleast 8 characters, a number and special character.";
+        }
+        
+        this.storedUsername = username;
+        this.storedPassword = password;
+        this.storedFirstName = firstName;
+        this.storedLastName = lastName;
+        
+        return "The user has been successfully registered.";
+    }
+    
+    public boolean loginUser(String enteredUsername, String enteredPassword){
+        return enteredUsername.equals(storedUsername) && enteredPassword.equals(storedPassword);
+    }
+    
+    public String returnLoginStatus(boolean loginStatus) {
+        if (loginStatus) {
+            return "Welcome " + storedFirstName + ", " + storedLastName + "it is great to see you are back.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+   
+    }
 }
