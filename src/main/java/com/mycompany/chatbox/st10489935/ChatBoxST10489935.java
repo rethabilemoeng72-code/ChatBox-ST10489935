d131f6b40cf273c8688760ba0b66ee0e16fa6e56
rethabilemoeng-72code <rethabilemoeng72@gmail.com>
2026-09-17 14:39:21 +0200

    I created ChatBox-ST10489935.java file

---
+++ src/main/java/com/mycompany/chatbox/st10489935/ChatBoxST10489935.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.chatbox.st10489935;
+
+/**
+ *
+ * @author Student
+ */
+public class ChatBoxST10489935 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
