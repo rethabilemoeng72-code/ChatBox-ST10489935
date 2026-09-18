313b853031762380eef295af3662e34b2fb500b3
rethabilemoeng-72code <rethabilemoeng72@gmail.com>
2026-09-18 19:50:33 +0200

    I fixed LoginTest.java file, it had errors

--- src/test/java/com/mycompany/chatbox/st10489935/LoginTest.java
+++ src/test/java/com/mycompany/chatbox/st10489935/LoginTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.poechatbox;
+package com.mycompany.chatbox.st10489935;
 
 import org.junit.Before;
 import org.junit.Test;
